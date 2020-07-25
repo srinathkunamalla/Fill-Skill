de41121259ff4aae27c9d65174671c1f6a20154a
Srinath Kunamalla <s0k022y@homeoffice.wal-mart.com>
2020-07-24 20:58:49 -0500

    Login Page initial commit

---
+++ src/component/Login/login.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Login</div>
+        <div className="content">
+          <div className="form">
+          <div className="form-group">
+              <label htmlFor="companyName">Company Name</label>
+              <input type="text" name="companyName" placeholder="Company Name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Login
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
