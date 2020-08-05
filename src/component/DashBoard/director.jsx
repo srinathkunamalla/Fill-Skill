c5873c4c2db918bac70a9897b561eed9c37fa6d8
Srinath Kunamalla <s0k022y@homeoffice.wal-mart.com>
2020-08-05 11:06:54 -0500

    director page changes

--- src/component/DashBoard/director.jsx
+++ src/component/DashBoard/director.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Directors } from "../../api/directors";
+import { Card, CardColumns, Button, Modal } from "react-bootstrap";
 
 class Director extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {directors: [] };
+    this.state = { directors: [] };
   }
-  async componentWillMount() {
+
+  async componentDidMount() {
     const cid = this.props.match.params.cid
     console.log(cid)
     const directors = await Directors.getAll(cid)
-    this.setState({directors: directors})
+    console.log('directors', directors);
+    this.setState({ directors: directors })
     console.log(this.state.directors)
 
   }
@@ -20,14 +23,44 @@ class Director extends React.Component {
         <div>
           <h1>Dashboard</h1>
           <h1>You are on Director Page</h1>
-          <button type="button" onClick={() => {this.props.history.push(`${this.props.history.location.pathname}/director/chirag`)}} className="btn">
+          <button type="button" onClick={() => { this.props.history.push(`${this.props.history.location.pathname}/director/chirag`) }} className="btn">
             select one director
           </button>
+        </div>
+        <div className="row">
+
+        </div>
+        <div className="row">
+          <CardColumns className="m-5">
+            {this.state.directors.map((director, i) => {
+              return(
+                <Card
+                key={i}
+              className="mb-2"
+            >
+              <Card.Header>{director.name}</Card.Header>
+              <Card.Body>
+                <Card.Title>{director.id} </Card.Title>
+                <Card.Text>
+                  <strong>Organization:</strong>&nbsp;&nbsp;{director.org}
+            </Card.Text>
+              </Card.Body>
+              <Card.Footer className="text-muted">
+              <Button className="btn btn-sm" variant="outline-primary">Edit</Button> &nbsp;&nbsp;
+              <Button className="btn btn-sm" variant="outline-danger">Delete</Button>
+              </Card.Footer>
+            </Card>
+              );
+
+            })
+          }
+            
+          </CardColumns>
         </div>
       </div>
     );
   }
- 
+
 };
 
 export default Director;
